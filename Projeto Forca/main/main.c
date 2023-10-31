deaefb9118f6eae33c53711d7560bf8b60019ebe
Luiz Felipe Soriano <132586339+lfelipesbs@users.noreply.github.com>
2023-10-30 21:48:09 -0300

    Add files via upload
    Commit inicial, com os primeiros arquivos e a biblioteca CLI.

---
+++ Projeto Forca/main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "forca.c"
+#include "keyboard.c"
+#include "screen.c"
+#include "timer.c"
+
+int main(void){
+    
+}
