c699d3ec0f0f03c0fa72f72d7227b465c933f8ac
Luiz Felipe Soriano <132586339+lfelipesbs@users.noreply.github.com>
2023-11-01 09:28:02 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "forca.c"
+#include "cli-lib-main/keyboard.c"
+#include "cli-lib-main/screen.c"
+#include "cli-lib-main/timer.c"
+
+char palavrasecreta[TAMANHO_PALAVRA];
+
+int main(void){
+    
+}
