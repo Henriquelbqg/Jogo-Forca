cc5da75864a5fb09145775266a9ffa4f7b18ec86
Henrique Lobo <133917236+Henriquelbqg@users.noreply.github.com>
2023-11-22 12:08:40 -0300

    Add files via upload

---
+++ bonecos.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void morto()
+{
+    printf("    _____         /        /\n");
+    printf("   | x    |      /        /\n");
+    printf("   | x    |---------------\n");
+    printf("   |______|      \\        \\\n");
+    printf("                  \\        \\\n");
+}
+
+void vivo()
+{
+    printf("    _________   \n");
+    printf("   |  O   O  |\n");
+    printf("   |    v    |\n");
+    printf("   |  \\____/ |\n");
+    printf("   |_________| \n");
+    printf("        |\n");
+    printf("        |\n");
+    printf("       /|\\\n");
+    printf("      / | \\\n");
+    printf("     /  |  \\\n");
+    printf("        |\n");
+    printf("       / \\\n");
+    printf("      /   \\\n");
+    printf("     /     \\\n");
+}
+
+int main (){
+
+    vivo();
+    
+    return 0;
+}
