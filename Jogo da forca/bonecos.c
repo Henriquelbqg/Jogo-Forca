6a8c7c7b06682741794df72202e59de9ddcf35b8
Henrique Lobo <133917236+Henriquelbqg@users.noreply.github.com>
2023-11-22 19:23:20 -0300

    Update bonecos.c

--- Jogo da forca/bonecos.c
+++ Jogo da forca/bonecos.c
@@ -3,46 +3,45 @@
 
 void morto() {
     printf("    _____         /        /\n");
-    usleep(500000);  
+    usleep(300000);  
     printf("   | x    |      /        /\n");
-    usleep(500000);
+    usleep(300000);
     printf("   | x    |---------------\n");
-    usleep(500000);
+    usleep(300000);
     printf("   |______|      \\        \\\n");
-    usleep(500000);
+    usleep(300000);
     printf("                  \\        \\\n");
-    usleep(500000);
+    usleep(300000);
 }
 
 void vivo() {
     printf("            _________   \n");
-    usleep(500000);
+    usleep(300000);
     printf("           |  O   O  |\n");
-    usleep(500000);
+    usleep(300000);
     printf("           |    v    |\n");
-    usleep(500000);
+    usleep(300000);
     printf("           |  \\____/ |\n");
-    usleep(500000);
+    usleep(300000);
     printf("           |_________| \n");
-    usleep(500000);
+    usleep(300000);
     printf("                |\n");
-    usleep(500000);
+    usleep(300000);
     printf("                |\n");
-    usleep(500000);
+    usleep(300000);
     printf("        ________|________\n");
-    usleep(500000);
+    usleep(300000);
     printf("                |   \n");
-    usleep(500000);
+    usleep(300000);
     printf("                |    \n");
-    usleep(500000);
+    usleep(300000);
     printf("                |\n");
-    usleep(500000);
+    usleep(300000);
     printf("               / \\\n");
-    usleep(500000);
+    usleep(300000);
     printf("              /   \\\n");
-    usleep(500000);
+    usleep(300000);
     printf("             /     \\\n");
 }
-
 
 
