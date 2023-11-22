cb86ead9bf6d9281c1ab470d58398ca603a122a7
Henrique Lobo <133917236+Henriquelbqg@users.noreply.github.com>
2023-11-22 15:26:37 -0300

    Add files via upload

---
+++ Jogo da forca/bonecos.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void morto()
+{
+    printf("    _____         /        /\n");
+    printf("   | x    |      /        /\n");
+    printf("   | x    |---------------\n");
+    printf("   |______|      \\        \\\n");
+    printf("                  \\        \\\n");
+}
+
+void vivo()
+{
+    printf("            _________   \n");
+    printf("           |  O   O  |\n");
+    printf("           |    v    |\n");
+    printf("           |  \\____/ |\n");
+    printf("           |_________| \n");
+    printf("                |\n");
+    printf("                |\n");
+    printf("        ________|________\n");
+    printf("                |   \n");
+    printf("                |    \n");
+    printf("                |\n");
+    printf("               / \\\n");
+    printf("              /   \\\n");
+    printf("             /     \\\n");
+}
+
