66dbe81f4926328acaa22bbae1d242afdcd2e8f8
Luiz Felipe Soriano <132586339+lfelipesbs@users.noreply.github.com>
2023-11-22 15:46:52 -0300

    Add files via upload

--- bonecos.c
+++ bonecos.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
 
-void morto()
-{
-    printf("    _____         /        /\n");
-    printf("   | x    |      /        /\n");
-    printf("   | x    |---------------\n");
-    printf("   |______|      \\        \\\n");
-    printf("                  \\        \\\n");
+void morto(){
+
+    printf("    _____         /        /    \n");
+    printf("   | x    |      /        /     \n");
+    printf("   | x    |---------------      \n");
+    printf("   |______|      \\        \\   \n");
+    printf("                  \\        \\  \n");
 }
 
-void vivo()
-{
+void vivo(){
+
     printf("    _________   \n");
-    printf("   |  O   O  |\n");
-    printf("   |    v    |\n");
-    printf("   |  \\____/ |\n");
-    printf("   |_________| \n");
-    printf("        |\n");
-    printf("        |\n");
-    printf("       /|\\\n");
-    printf("      / | \\\n");
-    printf("     /  |  \\\n");
-    printf("        |\n");
-    printf("       / \\\n");
-    printf("      /   \\\n");
-    printf("     /     \\\n");
+    printf("   |  O   O  |  \n");
+    printf("   |    v    |  \n");
+    printf("   |  \\____/ | \n");
+    printf("   |_________|  \n");
+    printf("        |       \n");
+    printf("        |       \n");
+    printf("       /|\\     \n");
+    printf("      / | \\    \n");
+    printf("     /  |  \\   \n");
+    printf("        |       \n");
+    printf("       / \\     \n");
+    printf("      /   \\    \n");
+    printf("     /     \\   \n");
 }
 
 int main (){
 
     vivo();
+    morto();
     
     return 0;
 }
