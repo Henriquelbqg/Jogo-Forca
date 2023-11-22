54c38e9079c1feb1c55fa61f843a9768b209dbf9
Henrique Lobo <133917236+Henriquelbqg@users.noreply.github.com>
2023-11-22 19:01:17 -0300

    boneco.pt2

---
+++ Jogo da forca/bonecos.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <unistd.h> 
+
+void morto() {
+    printf("    _____         /        /\n");
+    usleep(500000);  
+    printf("   | x    |      /        /\n");
+    usleep(500000);
+    printf("   | x    |---------------\n");
+    usleep(500000);
+    printf("   |______|      \\        \\\n");
+    usleep(500000);
+    printf("                  \\        \\\n");
+    usleep(500000);
+}
+
+void vivo() {
+    printf("            _________   \n");
+    usleep(500000);
+    printf("           |  O   O  |\n");
+    usleep(500000);
+    printf("           |    v    |\n");
+    usleep(500000);
+    printf("           |  \\____/ |\n");
+    usleep(500000);
+    printf("           |_________| \n");
+    usleep(500000);
+    printf("                |\n");
+    usleep(500000);
+    printf("                |\n");
+    usleep(500000);
+    printf("        ________|________\n");
+    usleep(500000);
+    printf("                |   \n");
+    usleep(500000);
+    printf("                |    \n");
+    usleep(500000);
+    printf("                |\n");
+    usleep(500000);
+    printf("               / \\\n");
+    usleep(500000);
+    printf("              /   \\\n");
+    usleep(500000);
+    printf("             /     \\\n");
+}
+
+
+
